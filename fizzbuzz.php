27f1592ff3d651dc52f8860af5561fe374714ec9
jerald saenz <JandCo@jeralds-MacBook-Pro.local>
2015-11-04 17:49:23 -0600

    modifcations to fizzbuzz and for files

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,12 +1,12 @@
 <?php
 
-
+ 
 for ($i = 1; $i <= 100; $i +=1){
 	if ($i % 15 == 0){
 		echo "FIZZBUZZ".PHP_EOL;
 	}elseif ($i % 5 == 0) {
 		echo "BUZZ". PHP_EOL;
-	}elseif($i % 3 ==0){
+	}elseif($i % 3 == 0){
 		echo "FIZZ". PHP_EOL;	
 	}else {	
 		echo $i . PHP_EOL;
---
+++ for.php
@@ -0,0 +1,27 @@
+<?php
+
+do{
+	fwrite(STDOUT, 'Please enter a starting number. ');
+		$num1 = trim(fgets(STDIN));	
+} while(!is_numeric($num1));
+
+do{	
+	fwrite(STDOUT, 'Please enter an ending number. ');
+		$num2 = trim(fgets(STDIN));
+} while(!is_numeric($num2));
+
+fwrite(STDOUT, 'Please enter an incrementer. ');
+	$num3 = trim(fgets(STDIN));
+if (empty($num3)){
+	$num3 = 1;
+}
+
+if($num1 > $num2){
+	for($i = $num2; $i <= $num1; $i +=$num3){
+		echo $i . PHP_EOL;
+	}
+} else {
+	for($i = $num1; $i <= $num2; $i +=$num3){
+		echo $i . PHP_EOL;
+		}
+	}
