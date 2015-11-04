ecb55086d8251a2eb279c903b0326de68bcaad9a
jerald saenz <JandCo@jeralds-MacBook-Pro.local>
2015-11-04 11:19:23 -0600

    upgrades to high low game

--- high_low.php
+++ high_low.php
@@ -1,7 +1,7 @@
 <?php
 
 $answer = rand(1,100);
-
+$counter = 0;
  do {
 	fwrite(STDOUT, 'Guess? ');
 	$guess = trim(fgets(STDIN));
@@ -10,6 +10,6 @@ $answer = rand(1,100);
 		fwrite(STDOUT, "Higher". PHP_EOL);
 	} else if ($guess > $answer){
 		fwrite(STDOUT, "Lower" .PHP_EOL);
-	}
+	} $counter +=1;
 } while ($guess != $answer);
-		fwrite(STDOUT, "Congratulations you guessed the number!!!!" . PHP_EOL);
+		fwrite(STDOUT, "Congratulations you guessed the number in ". $counter . " guesses !!!!" . PHP_EOL);
