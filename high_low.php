cfc2491e709b146c2f666ec059fef272d4a7d3f8
jerald saenz <JandCo@jeralds-MacBook-Pro.local>
2015-11-04 11:38:18 -0600

    upgrades to high low, testing for numeric and non numeric entries

--- high_low.php
+++ high_low.php
@@ -1,15 +1,20 @@
 <?php
 
-$answer = rand(1,100);
+$answer = mt_rand(1,100);
 $counter = 0;
  do {
 	fwrite(STDOUT, 'Guess? ');
 	$guess = trim(fgets(STDIN));
-	$guess = intval($guess);
-	if ($guess < $answer){
-		fwrite(STDOUT, "Higher". PHP_EOL);
-	} else if ($guess > $answer){
-		fwrite(STDOUT, "Lower" .PHP_EOL);
-	} $counter +=1;
+	if (!is_numeric($guess)){
+		fwrite(STDOUT, 'Please input a number!'.PHP_EOL);
+	}else {
+		$guess = intval($guess);
+		if ($guess < $answer){
+			fwrite(STDOUT, "Higher". PHP_EOL);
+		} else if ($guess > $answer){
+			fwrite(STDOUT, "Lower" .PHP_EOL);
+		}
+	$counter +=1;
+	}
 } while ($guess != $answer);
 		fwrite(STDOUT, "Congratulations you guessed the number in ". $counter . " guesses !!!!" . PHP_EOL);
