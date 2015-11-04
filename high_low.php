1533b6f0194e49bcbbe25bc97b82e158a2d063e0
jerald saenz <JandCo@jeralds-MacBook-Pro.local>
2015-11-04 13:35:31 -0600

    adding argc and argv to high low file

--- high_low.php
+++ high_low.php
@@ -1,20 +1,27 @@
 <?php
+if ($argc < 3) {
+	die("There are not enough arguments to run this program!!!!!". PHP_EOL);
+}
 
-$answer = mt_rand(1,100);
+$min = $argv[1];
+$max = $argv[2];
+
+
+$answer = mt_rand($min,$max);
 $counter = 0;
  do {
 	fwrite(STDOUT, 'Guess? ');
 	$guess = trim(fgets(STDIN));
 	if (!is_numeric($guess)){
-		fwrite(STDOUT, 'Please input a number!'.PHP_EOL);
-	}else {
-		$guess = intval($guess);
-		if ($guess < $answer){
-			fwrite(STDOUT, "Higher". PHP_EOL);
-		} else if ($guess > $answer){
-			fwrite(STDOUT, "Lower" .PHP_EOL);
+			fwrite(STDOUT, 'Please input a number!'.PHP_EOL);
+	} else {
+			$guess = intval($guess);
+			if ($guess < $answer){
+				fwrite(STDOUT, "Higher". PHP_EOL);
+			} else if ($guess > $answer){
+				fwrite(STDOUT, "Lower" .PHP_EOL);
+			}
+		$counter +=1;
 		}
-	$counter +=1;
-	}
-} while ($guess != $answer);
+	} while ($guess != $answer);
 		fwrite(STDOUT, "Congratulations you guessed the number in ". $counter . " guesses !!!!" . PHP_EOL);
