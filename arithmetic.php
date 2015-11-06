19456e1c968243790ad2cfad96ac2bc20423ecf3
jerald saenz <JandCo@jeralds-MacBook-Pro.local>
2015-11-06 09:25:40 -0600

    changes to arithmetic exercise functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,45 @@
 <?php
+$a = 10;
+$b = 5;
 
 function add($a, $b)
 {
     return $a + $b;
 }
+echo add(4,3).PHP_EOL;
 
 function subtract($a, $b)
 {
-    // Add code here
+    return $a - $b;
 }
+
+echo subtract(8,4).PHP_EOL;
 
 function multiply($a, $b)
 {
-    // Add code here
+    return $a * $b;
+    
 }
+echo multiply(7,5).PHP_EOL;
 
 function divide($a, $b)
 {
-    // Add code here
+   return $a/$b;
 }
+echo divide(16,4).PHP_EOL;
 
-// Add code to test your functions here
+function multiply2($a,$b) 
+{   // bonus: refactor to use your add() and a for loop
+	$c =0;
+    for($i = 1; $i <=$b; $i +=1){
+    		$c +=$a;
+    	}
+    return $c;
+ }
+ echo multiply2(7,5).PHP_EOL;
+
+ function modulus($a, $b)
+ {
+ 	return $a % $b;
+ }
+ echo modulus(4,3).PHP_EOL;
