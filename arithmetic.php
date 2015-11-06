ae17b9334302df2486b73a357aaf66944a577ccf
jerald saenz <JandCo@jeralds-MacBook-Pro.local>
2015-11-06 11:14:37 -0600

    function error exercises

--- arithmetic.php
+++ arithmetic.php
@@ -4,42 +4,76 @@ $b = 5;
 
 function add($a, $b)
 {
-    return $a + $b;
+   if(is_numeric($a) && is_numeric($b)){
+    	return $a + $b;
+    } else {
+    	return errorMessage($a,$b);
+    }
 }
-echo add(4,3).PHP_EOL;
 
 function subtract($a, $b)
 {
-    return $a - $b;
+ 	if(is_numeric($a) && is_numeric($b)){
+    	return $a - $b;
+    } else {
+    	return errorMessage($a,$b);
+    }
 }
 
-echo subtract(8,4).PHP_EOL;
 
 function multiply($a, $b)
 {
-    return $a * $b;
+    if(is_numeric($a) && is_numeric($b)){
+    	return $a * $b;
+    } else {
+    	return errorMessage($a,$b);
+    }
     
 }
-echo multiply(7,5).PHP_EOL;
 
 function divide($a, $b)
 {
-   return $a/$b;
+  if($b==0){
+  	return "ERROR: you can not have 0 as a divisor\n";
+  } elseif(is_numeric($a) && is_numeric($b)){
+    	return $a/$b;
+    } else {
+    	return errorMessage($a,$b);
+    }
 }
-echo divide(16,4).PHP_EOL;
 
 function multiply2($a,$b) 
 {   // bonus: refactor to use your add() and a for loop
+	if(is_numeric($a) && is_numeric($b)){
 	$c =0;
     for($i = 1; $i <=$b; $i +=1){
     		$c +=$a;
     	}
     return $c;
+	} else {
+		return errorMessage($a,$b);
+	}
  }
- echo multiply2(7,5).PHP_EOL;
 
  function modulus($a, $b)
- {
- 	return $a % $b;
+ {	
+ 	if($b==0){
+ 	return "ERROR: you can not have 0 as a divisor\n";
+ 	} elseif(is_numeric($a) && is_numeric($b)){
+    	return $a % $b;
+    } else {
+    	return errorMessage($a,$b);
+    }
  }
- echo modulus(4,3).PHP_EOL;
+
+ function errorMessage($a,$b)
+ {
+	  return 'ERROR: Please input only numbers $a is '. $a . ' $b is '. $b. PHP_EOL;
+ }
+
+echo add(4,3).PHP_EOL;
+echo subtract(8,4).PHP_EOL;
+echo multiply(7,5).PHP_EOL;
+echo divide(16,4).PHP_EOL;
+echo modulus(4,3).PHP_EOL;
+echo multiply2(7,5).PHP_EOL;
