3c836bd83c3174e47ddcdc38fc089752d2e15b0b
jerald saenz <JandCo@jeralds-MacBook-Pro.local>
2015-11-09 12:38:06 -0600

    adding merge arrays exercise

---
+++ merge-arrays.php
@@ -0,0 +1,30 @@
+<?php
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function combineArrays($array1, $array2){
+	$newArray = [];
+	$arrayLength = count($array1);
+	for ($i = 0; $i <= ($arrayLength -1); $i++)
+		{
+			if ($array1[$i] === $array2[$i]){
+			array_push($newArray,$array1[$i]);
+		} 	else {
+			array_push($newArray, $array1[$i]);
+			array_push($newArray, $array2[$i]);
+		}
+		} print_r($newArray).PHP_EOL;
+	 }
+
+combineArrays($names, $compare);
+
+	
+
+
+
+	// $difference = array_diff($array1, $array2);
+	// var_dump($difference);
+	// $new = array_push($array1, $difference);
+	// print_r($new).PHP_EOL;
